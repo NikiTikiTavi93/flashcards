38b4357e76ee9f40dc024afebf48a571868cfba0
NikiTikiTavi93 <alerto_93@mail.ru>
2016-09-24 22:27:26 +0300

    fix model, add validate not equal texts

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,15 @@
 class Card < ApplicationRecord
   before_save {original_text.downcase!}
   before_save {translated_text.downcase!}
-  before_save :review_date
-  validates :original_text, :translated_text, :review_date, presence: true
+  before_create :review_date
+  validates :original_text, :translated_text, :review_date,
+            presence: true
+
+  validate :original_text_not_equal_translated_text
+
+  def original_text_not_equal_translated_text
+    errors.add(:original_text, "Original text not equal translated") if original_text == translated_text
+  end
 
   def review_date
     date = Date.today + 3
