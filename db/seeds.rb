70921c3e2b5e5527c46469884379164607e07606
NikiTikiTavi93 <alerto_93@mail.ru>
2016-09-27 07:54:27 +0300

    fixed seeds.rb and Time notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,13 @@ require 'mechanize'
 agent = Mechanize.new
 PAGE_URL = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 page = agent.get(PAGE_URL)
-links = page.links_with(text: %r{Most.*})
+links = page.links_with(text: /Most.*/)
 links.map do |link|
   word = link.click
   word.css('tr').each do |td|
     begin
       puts Card.create!(original_text: td.css('td')[1].content,
-                   translated_text: td.css('td')[2].content,
-                   review_date: Time.zone.today)
+                   translated_text: td.css('td')[2].content)
     rescue ActiveRecord::RecordInvalid
     end
   end
