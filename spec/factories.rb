c608df3f08915dcf243d640a9f850efc8db6565c
NikiTikiTavi93 <alerto_93@mail.ru>
2016-11-06 18:35:26 +0300

    Notifications spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,7 +15,5 @@ FactoryGirl.define do
     original_text "text"
     translated_text "trans_text"
     review_date {5.days.ago}
-    # count_checks 0
-    # count_errors 0
   end
 end
--- spec/mailers/notifications_mailer_spec.rb
+++ spec/mailers/notifications_mailer_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
-RSpec.describe NotificationsMailerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe NotificationsMailer, type: :mailer do
+  describe "notify" do
+    let(:mail) {NotificationsMailer.pending_cards}
+    let(:user) {FactoryGirl.create(:user)}
+    let(:deck) {FactoryGirl.create(:deck)}
+    let(:card) {FactoryGirl.create(:card, deck_id: deck.id)}
+    it "test mailer" do
+      puts card
+      expect(mail.subject).to eq('There were cards available for testing')
+    end
+  end
 end
