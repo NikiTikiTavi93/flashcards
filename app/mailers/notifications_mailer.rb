7af8d33018450e570df7ebb63b512cb08eddbecb
NikiTikiTavi93 <alerto_93@mail.ru>
2016-12-07 17:29:41 +0300

    add mandrill

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,11 +1,17 @@
-class NotificationsMailer < ApplicationMailer
+class NotificationsMailer < BaseMandrillMailer
   def pending_cards
     user_ids = Card.select(:user_id)
                    .where("review_date <=?", DateTime.now)
                    .group(:user_id)
                    .pluck(:user_id)
+
     User.where(id: user_ids).all.each do |user|
-        mail(to: user.email, subject: 'There were cards available for testing')
+      subject = 'There were cards available for testing'
+      merge_vars = {
+          "Email" => user.email
+      }
+      body = mandrill_template("pending-cards", merge_vars)
+      send_mail(user.email, subject, body)
     end
   end
 end
